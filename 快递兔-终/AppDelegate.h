ec376e6e1ba3c310b0606593684d591b1acfbd1e
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2014-11-10 20:04:36 +0800

    Initial Commit

---
+++ 快递兔-终/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  快递兔-终
+//
+//  Created by kuaiditu on 14-11-10.
+//  Copyright (c) 2014年 kuaiditu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
